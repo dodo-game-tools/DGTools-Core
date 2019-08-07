c8e2a5fc3f1deb9cdc5b26a4dccaa8db2f82d5f2
Donovan <donovan.persent@gmail.com>
2019-08-07 14:06:27 +0200

    Add IFillable

---
+++ Runtime/Scripts/Interfaces/IFillable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+public interface IFillable<Titem>
+{
+    void AddItem(Titem item);
+    void AddItems(Titem[] item);
+    void RemoveItem(Titem item);
+    List<Titem> GetItems();
+}
